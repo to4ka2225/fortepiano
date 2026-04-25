7586a136cb5e08ceceb50cea5fa918f3300ecfb9
to4ka2225 <to4ka5464@gmail.com>
2026-04-25 15:18:47 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,27 +1,46 @@
 import pygame
-from settings import*
+from settings import *
 from ui import Button
 
 def main():
+    # Ініціалізація
     pygame.init()
     pygame.display.set_caption("Фортепіано")
-    screen = pygame.display.set_mode((WIDTH,HEIGHT))
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
     clock = pygame.time.Clock()
 
+    # Створення клавіш (кнопок)
     keys = []
     key_width = WIDTH // 8
 
     for i in range(7):
+        # Розраховуємо позицію кожної клавіші
         x_pos = i * key_width + 45
-        sound_path = SOUNDS_FILES[i]
+        # Беремо звук зі списку (якщо звуків менше ніж 8, використовуємо остачу від ділення)
+        sound_path = SOUND_FILES[i]
 
         btn = Button(
-            x=x_pos,y=100,width=key_width - 2,height=250,color=WHITE,sound_path=sound_path
-        )
+            x=x_pos, y=100, width=key_width - 2,
+            height=250, color=WHITE, sound_path=sound_path)
         keys.append(btn)
 
+    # Головний цикл програми
     while True:
         screen.fill(GRAY)
 
         for event in pygame.event.get():
-            if event
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                exit()
+            # Обробка подій для кожної клавіші
+            for key in keys:
+                key.handle_event(event)
+        # Малювання клавіш
+        for key in keys:
+            key.draw(screen)
+
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+main()
--- ui.py
+++ ui.py
@@ -5,10 +5,13 @@ class Button:
         self.rect = pygame.Rect(x,y,width,height)
         self.color = color
         self.original_color = color
+
         self.sound = pygame.mixer.Sound(sound_path)
 
     def draw(self,surface):
+
         pygame.draw.rect(surface,self.color,self.rect,border_radius=15)
+
         pygame.draw.rect(surface,(0,0,0),self.rect,2,border_radius=15)
 
     def handle_event(self,event):
@@ -20,4 +23,35 @@ class Button:
         if event.type == pygame.MOUSEBUTTONUP:
             self.color = self.original_color
 
+        return False
+
+class Slider:
+    def __init__(self,x,y,w,h,val=0.5):
+        self.rect = pygame.Rect(x,y,w,h)
+        self.circle_x = x +int(w + val)
+        self.val = val
+        self.dragging = False
+
+    def draw(self,surfase):
+        pygame.draw.line(surfase,(0,0,0),
+                         (self.rect.x,self.rect.centery),
+                         (self.rect.right,self.rect.centery),4)
+        pygame.draw.circle(surfase,(100,149,237),(self.circle_x,self.rect.centery),10)
+
+    def handle_event(self,event):
+        if event.type == pygame.MOUSEBUTTONDOWN and (
+                (event.pos[0] - self.circle_x) ** 2 + (
+                event.pos[1] - self.rect.centery) ** 2
+        ) ** 0.5 < 15:
+            self.dragging = True
+        elif event.type == pygame.MOUSEBUTTONUP:
+            self.dragging = False
+
+        if self.dragging and event.type == pygame.MOUSEMOTION:
+            self.circle_x = max(self.rect.left,
+                                min(event.pos[0],self.rect.right))
+            self.val = (self.circle_x - self.rect.x
+                        ) / self.rect.width
+
+            return True
         return False
